4679ef8a17a836eeffd9b9cef1fda633e0a856ef
Grunet <sadikshahidain@gmail.com>
2020-11-08 08:00:55 -0600

    Removing the S3 retrieval code now that the bucket containing the emails is no longer in use

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,4 @@
-const {
-  getSubscribers: getSubscribersFromMailchimp,
-} = require("../src/index.js");
-const {
-  getSubscribers: getSubscribersFromAWS,
-} = require("../src/getSubscribers.js");
-
-async function getSubscribers(inputs) {
-  if (inputs?.hasOwnProperty("mailchimp")) {
-    return await getSubscribersFromMailchimp(inputs);
-  }
-
-  return await getSubscribersFromAWS();
-}
+const { getSubscribers } = require("../src/index.js");
 
 module.exports = {
   getSubscribers: getSubscribers,
